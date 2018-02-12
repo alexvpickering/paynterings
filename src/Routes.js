b4a756273e6929b4764f9ffa0704fd8003cf628d
alexvpickering <alexvpickering@gmail.com>
2018-02-11 17:43:16 -0800

    redirect based on authentication

--- src/Routes.js
+++ src/Routes.js
@@ -3,15 +3,37 @@ import { Route, BrowserRouter as Router } from "react-router-dom";
 import HomePage from "./components/HomePage/HomePage";
 import LoginPage from "./components/LoginPage/LoginPage";
 import AddPage from "./components/AddPage/AddPage";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import AuthenticatedRoute from "./components/AuthenticatedRoute";
+import UnauthenticatedRoute from "./components/UnauthenticatedRoute";
 
-export default () => {
+const Routes = ({ isAuthenticated }) => {
   return (
     <Router>
       <div>
         <Route exact path="/" component={HomePage} />
-        <Route exact path="/login" component={LoginPage} />
-        <Route exact path="/add" component={AddPage} />
+        <UnauthenticatedRoute
+          exact
+          path="/login"
+          component={LoginPage}
+          props={{ isAuthenticated }}
+        />
+        <AuthenticatedRoute
+          exact
+          path="/add"
+          component={AddPage}
+          props={{ isAuthenticated }}
+        />
       </div>
     </Router>
   );
 };
+
+Routes.propTypes = {
+  isAuthenticated: PropTypes.bool.isRequired
+};
+
+export default connect(state => ({
+  isAuthenticated: state.isAuthenticated
+}))(Routes);
